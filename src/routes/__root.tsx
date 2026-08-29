9cc85398686b52c027a0f6216b9d5129e648dbb6
Elliot Luna <elliot@MacBookPro.hsd1.wa.comcast.net>
2026-08-28 20:29:19 -0700

    Add Google Analytics (gtag.js) tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -104,6 +104,16 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R3S5BQYLJW" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-R3S5BQYLJW');`,
+          }}
+        />
         <HeadContent />
         <script
           dangerouslySetInnerHTML={{
